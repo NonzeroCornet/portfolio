01b875b5019891dcfea6e28bf0425be3add9aa98
NonzeroCornet <ullerbri000@gmail.com>
2023-12-04 21:53:00 +0000

    fix visual glitch

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
-var spanLinks = document.querySelector(".about-content").getElementsByTagName("span");
-for (let i = 0; i < spanLinks.length; i++) {
-  spanLinks[i].addEventListener("click", (event) => {
-    document.body.classList.add("shake");
-    setTimeout(() => {
-      document.body.classList.remove("shake");
-    }, 500);
-  });
-}
-
 if (window.innerWidth <= 800 || window.innerHeight <= 600) {
   document.getElementById("in").innerHTML = ".";
 } else {
